7391450ef2841f00019d67de7278ce3dbe325ab9
Crystal Csete <jeannecc13@gmail.com>
2021-07-26 14:55:30 -0600

    Adds files to repo

---
+++ src/App.js
@@ -0,0 +1,13 @@
+/** @format */
+
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>App file</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Winner.js
@@ -0,0 +1,13 @@
+/** @format */
+
+import React from "react";
+
+const Winner = () => {
+  return (
+    <div>
+      <h2>winner file</h2>
+    </div>
+  );
+};
+
+export default Winner;
